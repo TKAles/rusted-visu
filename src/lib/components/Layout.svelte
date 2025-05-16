56ca95b973b0f5191a84fadd45c316a01e9c8584
Thomas Ales [M S E] <tkales@iastate.edu>
2025-05-16 07:42:32 -0500

    tauri framework

---
+++ src/lib/components/Layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let title = "VISU | Rusted Edition";
+</script>
+	
+<div class="min-h-screen bg-gray-50">
+	<!--- Header -->
+	<header class="bg-white shadow">
+		<div class="container mx-auto px-4 py-4">
+			<h1>{title}</h1>	
+		</div>
+	</header>
+	<!-- Main Window -->
+	 <main class="container mx-auto px-4 py-4">
+		<slot />
+	</main>
+	<footer></footer>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import Layout from '$lib/components/Layout.svelte';
+</script>
+
+<Layout title="VISU | Rusted Edition">
+<div>
+    put some shit here
+</div>
+</Layout>
