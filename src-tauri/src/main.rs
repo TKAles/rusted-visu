1cf070684742d8d87cee69e0c6ec4a84e5a62cdc
Thomas Ales [M S E] <tkales@iastate.edu>
2025-05-19 21:03:07 -0500

    💩 🔬 💽

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,32 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use dirs::desktop_dir;
+use std::path::PathBuf;
+use tauri::{generate_handler, Window};
+use tauri_plugin_dialog::DialogExt;
 
 fn main() {
-  app_lib::run();
+  tauri::Builder::default()
+    .plugin(tauri_plugin_dialog::init())
+    .invoke_handler(generate_handler![select_wfm_folder])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
+
+#[tauri::command]
+pub async fn select_wfm_folder(window: Window) ->
+  Result<Option<String>, String> {
+    let desktop_dir = desktop_dir().unwrap_or_else(|| PathBuf::from("."));
+    let selected_folder = window
+          .dialog()
+          .file()
+          .set_directory(desktop_dir)
+          .pick_folder()
+          .await;
+
+    match selected_folder {
+      Some(folder_path) => Ok(Some(folder_path.to_string_lossy().to_string())),
+      None => Ok(None),
+
+    }
 }
--- src/lib/components/Layout.svelte
+++ src/lib/components/Layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import Modal from './Modal.svelte';
+	
 
-	export let title = "VISU | Rusted Edition";
+	import Modal from './Modal.svelte';
 	export let isPropertiesOpen: boolean = false;
 	export let WFMFolderSelected: boolean = false;
 	export let DCScansProcessed: boolean = false;
@@ -26,7 +26,7 @@
 </script>
 	
 <main class="flex w-full h-screen bg-gray-200 text-gray-900">
-	<aside class="w-1/3 bg-gray-600 h-full p-4 flex flex-col">
+	<aside class="w-1/3 bg-gray-600 h-full p-4 flex flex-col max-w-64">
 	<button on:click={selectWFMFolder} class="btn-sidebar">Open WFM Folder</button>
 	<button on:click={launchScanCheck} class="{!WFMFolderSelected ? 'btn-sidebar-disabled' : 'btn-sidebar'}"
 	disabled={!WFMFolderSelected}>
@@ -34,16 +34,17 @@
 	<button on:click={processDCScans} class="{!WFMFolderSelected ? 'btn-sidebar-disabled' : 'btn-sidebar'}" 
 	disabled={!WFMFolderSelected}>
 		Process DC Maps</button>
+	<button class="{!DCScansProcessed ? 'btn-sidebar-disabled' : 'btn-sidebar'}">
+		Export DC CSV</button>
 	<button on:click={processFFTs} class="{!DCScansProcessed ? 'btn-sidebar-disabled' : 'btn-sidebar'}" 
 	disabled={!DCScansProcessed}>
 		Process FFT Maps</button>
+	<button class="{!FFTsComputed ? 'btn-sidebar-disabled' : 'btn-sidebar'}">Export FFT CSV</button>
 	<button on:click={togglePropertieModal} class="{!WFMFolderSelected ? 'btn-sidebar-disabled' : 'btn-sidebar'}"
-	disabled={!WFMFolderSelected} 
-	>
-		Edit Scan Properties</button>			
+	disabled={!WFMFolderSelected}>Edit Scan Properties</button>			
 	</aside>
 	<section class="w-2/3 h-full p-4 flex flex-col gap-2">
 		<slot />
 	</section>
 </main>
-<Modal isOpen={isPropertiesOpen} onClose={togglePropertieModal} />
+<Modal isOpen={isPropertiesOpen} onClose={togglePropertieModal}/>
